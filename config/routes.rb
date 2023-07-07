6f479c6ab0d0a6c5b410b99d6e3a457fba51e7c5
ninomiya <hoshi35503002@gmail.com>
2023-07-07 02:14:36 +0000

    トップベージ途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'static_pages/top'
-
+  get '/signup', to: 'users#new'
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  # get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
