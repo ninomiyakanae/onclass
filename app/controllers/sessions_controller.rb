0acef45cd4d0bdaed644116263977c9266eeb1c0
ninomiya <hoshi35503002@gmail.com>
2023-08-15 09:06:23 +0000

    6. 2. 2 ユーザーの検索と認証

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render :new
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ログイン後にユーザー情報ページにリダイレクトします。
+    else
+      # ここにはエラーメッセージ用のflashを入れます。
+      render :new
+    end
   end
 end
