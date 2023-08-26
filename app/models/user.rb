057f66157887f5cc215d7900dc6ccb03f38ea685
ninomiya <hoshi35503002@gmail.com>
2023-08-26 16:12:48 +0000

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,10 +34,12 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  # トークンがダイジェストと一致すればtrueを返します。
-  def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
+# トークンがダイジェストと一致すればtrueを返します。
+def authenticated?(remember_token)
+  # ダイジェストが存在しない場合はfalseを返して終了します。
+  return false if remember_digest.nil?
+  BCrypt::Password.new(remember_digest).is_password?(remember_token)
+end
 
   # ユーザーのログイン情報を破棄します。
   def forget
